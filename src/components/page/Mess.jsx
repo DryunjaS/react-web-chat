f15bf14c2ca5abd77f2f242a7c1d2fedf5d821a5
Sosnin Andrey <KamushekKamushek@gmail.com>
2024-02-11 00:01:01 +0300

    fix: css

--- src/components/page/Mess.jsx
+++ src/components/page/Mess.jsx
@@ -1,71 +1,88 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 export const Mess = ({ message, User, baseServerUrl, thisChat }) => {
 	const filteredMessages = message.filter((mess) => mess.room === thisChat)
+	const lastMessageRef = useRef(null)
+
+	const scrollToLastMessage = () => {
+		lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' })
+	}
+
+	useEffect(() => {
+		scrollToLastMessage()
+	}, [filteredMessages])
+
 	return (
 		<div>
 			{filteredMessages.map((mess, index) => (
-				<ul className='ul-mess' key={index}>
-					<li className={User === mess.nick ? 'mess-li-my' : 'mess-li-other'}>
-						<div
-							className={
-								User === mess.nick ? 'mess-title-my' : 'mess-title-other'
-							}
-						>
-							{User === mess.nick ? 'Вы' : `${mess.nick}`}
-						</div>
-						<div className={User === mess.nick ? 'mess-div-wrap' : ''}>
+				<div
+					key={index}
+					ref={index === filteredMessages.length - 1 ? lastMessageRef : null}
+				>
+					<ul className='ul-mess'>
+						<li className={User === mess.nick ? 'mess-li-my' : 'mess-li-other'}>
 							<div
 								className={
-									User === mess.nick ? 'mess-div-my' : 'mess-div-other'
+									User === mess.nick ? 'mess-title-my' : 'mess-title-other'
 								}
 							>
-								{mess.isFile ? (
-									mess.fileType.startsWith('image/') ? (
-										<div>
-											<img
-												src={`${baseServerUrl}${mess.downloadLink}`}
-												alt={mess.fileName}
-												style={{ maxWidth: '200px', maxHeight: '200px' }}
-											/>
-											<br />
-											<a
-												href={`${baseServerUrl}${mess.downloadLink}`}
-												download={mess.fileName}
-												target='_blank'
-												rel='noopener noreferrer'
-											>
-												Download Image
-											</a>
-										</div>
-									) : (
-										<div>
-											<a
-												href={`${baseServerUrl}${mess.downloadLink}`}
-												download={mess.fileName}
-												target='_blank'
-												rel='noopener noreferrer'
-											>
-												{mess.fileName}
-											</a>
-										</div>
-									)
-								) : (
-									<>
-										<div className='mess-text'>{mess.message}</div>
-										<div
-											className={
-												User === mess.nick ? 'mess-time-my' : 'mess-time-other'
-											}
-										>
-											{mess.time}
-										</div>
-									</>
-								)}
+								{User === mess.nick ? 'Вы' : `${mess.nick}`}
 							</div>
-						</div>
-					</li>
-				</ul>
+							<div className={User === mess.nick ? 'mess-div-wrap' : ''}>
+								<div
+									className={
+										User === mess.nick ? 'mess-div-my' : 'mess-div-other'
+									}
+								>
+									{mess.isFile ? (
+										mess.fileType.startsWith('image/') ? (
+											<div>
+												<img
+													src={`${baseServerUrl}${mess.downloadLink}`}
+													alt={mess.fileName}
+													style={{ maxWidth: '200px', maxHeight: '200px' }}
+												/>
+												<br />
+												<a
+													href={`${baseServerUrl}${mess.downloadLink}`}
+													download={mess.fileName}
+													target='_blank'
+													rel='noopener noreferrer'
+												>
+													Download Image
+												</a>
+											</div>
+										) : (
+											<div>
+												<a
+													href={`${baseServerUrl}${mess.downloadLink}`}
+													download={mess.fileName}
+													target='_blank'
+													rel='noopener noreferrer'
+												>
+													{mess.fileName}
+												</a>
+											</div>
+										)
+									) : (
+										<>
+											<div className='mess-text'>{mess.message}</div>
+											<div
+												className={
+													User === mess.nick
+														? 'mess-time-my'
+														: 'mess-time-other'
+												}
+											>
+												{mess.time}
+											</div>
+										</>
+									)}
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
 			))}
 		</div>
 	)
